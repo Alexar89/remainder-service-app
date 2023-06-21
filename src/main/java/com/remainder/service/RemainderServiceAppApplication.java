85d5066211997c71979e8760a8bbf753698642c8
Alexar_89 <alexar89@gmail.com>
2023-06-21 10:43:18 -0500

    initial commit for project

---
+++ src/main/java/com/remainder/service/RemainderServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.remainder.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemainderServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemainderServiceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/remainder/service/RemainderServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remainder.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemainderServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
